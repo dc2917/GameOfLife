38565f16e1a90ac183d2feba59cd31c4024d8642
Daniel Cummins <daniel.cummins17@imperial.ac.uk>
2023-10-30 21:42:29 +0000

    Safely close matplotlib figure on state creation window close

--- configwindow.py
+++ configwindow.py
@@ -25,32 +25,30 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 class GameWindow(QWidget):
     def __init__(self, world):
         super().__init__()
-        self.setWindowTitle("Game of Life")
-        layout = QGridLayout()
         self._world = world
         self._create_mpl_figure()
-        layout.addWidget(self._canvas)
-        self.setLayout(layout)
         self.run()
 
     def _create_mpl_figure(self):
-        self._fig = plt.figure()
-        self._ax = plt.axes(xticks=[], yticks=[])
-        self._im = self._ax.imshow(self._world._grid.cells(), cmap="binary", vmin=0, vmax=1)
-        self._canvas = FigureCanvasQTAgg(self._fig)
+        plt.figure()
+        ax = plt.axes(xticks=[], yticks=[])
+        ax.figure.canvas.manager.set_window_title("Game of Life")
+        ax.figure.canvas.manager.toolbar.hide()
+        self._im = ax.imshow(self._world._grid.cells(), cmap="binary", vmin=0, vmax=1)
 
     def run(self):
         ni, nj = self._world._width, self._world._height
         plt.pause(self._world._tick)
-        for t in range(500):  # tick
+        finished = False
+        while not finished:
             copy = self._world._grid._cells.copy()
-            for i in range(1,ni-1):  # go across each row
-                for j in range(1,nj-1):  # go down each column
+            for i in range(1, ni-1):  # go across each row
+                for j in range(1, nj-1):  # go down each column
                     neighbourhood = copy[i-1:i+2, j-1:j+2]
                     num_live_neighbours = self._world._grid.count_live_neighbours(neighbourhood)
                     self._world._grid._cells[i, j] = self._world._grid.outcome(copy[i, j], num_live_neighbours)
             if not np.any(self._world._grid._cells-copy):
-                break
+                finished = True
             else:
                 self._im.set_data(self._world._grid._cells)
                 plt.pause(self._world._tick)
@@ -74,10 +72,10 @@ class CreateStateWindow(QWidget):
         self.show()
 
     def _create_mpl_figure(self):
-        self._fig = plt.figure()
-        self._ax = plt.axes(xticks=[], yticks=[])
-        self._im = self._ax.imshow(self._grid.cells(), cmap="binary", vmin=0, vmax=1)
-        self._canvas = FigureCanvasQTAgg(self._fig)
+        plt.figure()
+        ax = plt.axes(xticks=[], yticks=[])
+        self._im = ax.imshow(self._grid.cells(), cmap="binary", vmin=0, vmax=1)
+        self._canvas = FigureCanvasQTAgg(ax.figure)
         self._canvas.mpl_connect("button_press_event", self._on_canvas_clicked)
 
     def _on_canvas_clicked(self, event):
@@ -100,6 +98,9 @@ class CreateStateWindow(QWidget):
         self._grid.clear()
         self._im.set_array(self._grid.cells())
         self._canvas.draw()
+
+    def closeEvent(self, event):
+        plt.close()
 
 
 class WorldPropertiesPanel(QGroupBox):
