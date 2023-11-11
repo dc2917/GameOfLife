fc7645bec1ae06e04eb9cb16b19b03900b643a95
Daniel Cummins <daniel.cummins17@imperial.ac.uk>
2023-11-11 13:48:00 +0000

    Improve docs path definition

--- helpwindow.py
+++ helpwindow.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from PyQt6.QtCore import QUrl
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 from PyQt6.QtWidgets import QWidget, QVBoxLayout
@@ -6,8 +8,9 @@ class HelpWindow(QWidget):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("Documentation")
+        docs_path = Path("./docs/build/html/index.html")
         browser = QWebEngineView()
-        browser.load(QUrl("file:///home/dan/Documents/Python/GameOfLife/Qt/docs/build/html/index.html"))
+        browser.load(QUrl(f"file://{Path(__file__).parent}/{docs_path}"))
         layout = QVBoxLayout()
         layout.addWidget(browser)
         self.setLayout(layout)
